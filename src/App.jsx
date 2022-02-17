5067a104f9ebf2add25d88a2de6fa68042d2a91b
mbradley-github <76399993+mbradley-github@users.noreply.github.com>
2022-02-16 18:03:32 -0700

    Navbar layout, adding Material UI packages

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
+import Home from './pages/Home'
 
 const App = () => {
-  return (
-    <div>App</div>
-  )
+    return (
+        <Home />
+    )
 }
 
 export default App
