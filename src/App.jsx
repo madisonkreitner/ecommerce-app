1ee9f60e1ca24ce51ea7586de4891d8acdc7bfa1
mbradley-github <76399993+mbradley-github@users.noreply.github.com>
2022-02-17 14:10:56 -0700

    Login and register page layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import Home from './pages/Home'
+import Login from './pages/Login'
 import Product from './pages/Product'
 import ProductList from './pages/ProductList'
+import Register from './pages/Register'
 
 const App = () => {
     return (
-        <Product />
+        <Login />
     )
 }
 
