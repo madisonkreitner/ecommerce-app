3dcb20474d4cccdab754363a9e6f90f1a4633278
mbradley-github <76399993+mbradley-github@users.noreply.github.com>
2022-02-17 13:49:18 -0700

    Product Page layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import Home from './pages/Home'
+import Product from './pages/Product'
 import ProductList from './pages/ProductList'
 
 const App = () => {
     return (
-        <ProductList />
+        <Product />
     )
 }
 
