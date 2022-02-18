b6459c7158a633876862494b87b986e66bf5f853
mbradley-github <76399993+mbradley-github@users.noreply.github.com>
2022-02-17 17:12:18 -0700

    Cart page layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import Cart from './pages/Cart'
 import Home from './pages/Home'
 import Login from './pages/Login'
 import Product from './pages/Product'
@@ -7,7 +8,7 @@ import Register from './pages/Register'
 
 const App = () => {
     return (
-        <Login />
+        <Cart />
     )
 }
 
